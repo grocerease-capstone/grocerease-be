479ea7b8f4b17533a8329cf513b862120875539d
iBan <banozzgaming@gmail.com>
2024-11-20 14:14:26 +0700

    Initial commit, BE Template

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # grocerease-be-test
+
+Production Dependencies:
+npm install express dotenv mysql2 sequelize jsonwebtoken multer @google-cloud/storage
+
+Development Dependencies:
+npm init @eslint/config@latest
+npm install --save-dev eslint-config-dicodingacademy
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginPrettier from "eslint-plugin-prettier";
-import configAirbnb from "eslint-config-airbnb";
-import configPrettier from "eslint-config-prettier";
-
+import daStyle from 'eslint-config-dicodingacademy';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
- 
+  daStyle,
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-
-  configAirbnb,
-
-  {
-    plugins: {
-      prettier: pluginPrettier,
-    },
-    rules: {
-      "prettier/prettier": "error",
-    },
-  },
-
-  configPrettier,
 ];
