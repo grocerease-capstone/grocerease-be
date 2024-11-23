0add4df1a3cab44a6a1e994092b0da5186e6a8b3
Ban <113820586+Banozz@users.noreply.github.com>
2024-11-24 04:36:59 +0700

    Update orm.js

--- src/config/orm.js
+++ src/config/orm.js
@@ -3,11 +3,11 @@ import { Sequelize } from 'sequelize';
 
 const getSequelize = () => {
   const sequelize = new Sequelize(
-    process.env.DB_NAME || 'grocerease-db',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || '0416',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST || '127.0.0.1',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       port: process.env.DB_PORT || 3306,
 
