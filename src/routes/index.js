9d9ede01c26d54768d81887459c1da06fa4f9e17
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-10 20:11:55 +0700

    feat: Add Swagger documentation for share request routes and integrate Swagger UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
-﻿import { verifyToken } from "../middlewares/jwt.js";
+﻿import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { verifyToken } from "../middlewares/jwt.js";
 import authRoutes from "./auth_routes.js";
 import listRoutes from "./list_routes.js";
 import shareRequestRoutes from "./share_request_routes.js";
@@ -24,6 +26,35 @@ const appRoutes = (app) => {
   app.use("/list", listRoutes);
   app.use("/user", verifyToken, userRoutes);
   app.use("/share-request", shareRequestRoutes);
+
+  // swagger
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(
+      swaggerJSDoc({
+        definition: {
+          openapi: "3.1.0",
+          info: {
+            title: "GrocerEase API Documentation",
+            version: "1.0",
+            description:
+              "This is the API documentation for the GrocerEase application.",
+          },
+          components: {
+            securitySchemes: {
+              BearerAuth: {
+                type: "http",
+                scheme: "bearer",
+                bearerFormat: "JWT",
+              },
+            },
+          },
+        },
+        apis: ["./src/routes/*.js"],
+      })
+    )
+  );
 
   return app;
 };
