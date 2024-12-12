75f56eb6a2483cb18946c5e88839f7f3629af69b
iBan <banozzgaming@gmail.com>
2024-12-12 21:29:34 +0700

    Fix image fetching bug

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,11 @@ import { initializeApp, } from 'firebase-admin/app';
 import admin from 'firebase-admin';
 import { getMessaging } from 'firebase-admin/messaging';
 
-// const serviceAccount = process.env.FIREBASE_KEY_FILE;
-
 const app = initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FCM_PROJECT_ID,
     clientEmail: process.env.FCM_CLIENT_EMAIL,
-    privateKey: process.env.FCM_PRIVATE_KEY,
+    privateKey: (`${process.env.FCM_PRIVATE_KEY}`).replace(/\\n/g, '\n'),
   }),
   projectId: process.env.GC_PROJECT_ID,
 });
